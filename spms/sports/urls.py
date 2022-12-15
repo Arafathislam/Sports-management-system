eb7993b406ce5bb111f77234e53c015e5be17478
Hasnain <mrajhasnain101@gmail.com>
2022-12-15 23:35:48 +0600

    done some routing

--- spms/sports/urls.py
+++ spms/sports/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path,include
 from .import views
 
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('about/'views.about,name="about"),
+    path('about/',views.about,name="about"),
+    path('login/',views.login,name='login'),
+    path('forgotpass/',views.forgetpass,name='forgotpass')
+    # path('forgetpass/',views.forgetPass,name='forgetPass')
 
 
 ]
--- spms/sports/views.py
+++ spms/sports/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    context={}
-
-    return render(request,'sports/index.html',context)
-
+    return render(request,'sports/index.html')
 def about(request):
-    contxt={}
-    return render(request,'sports/about.html',context)
+    return render(request,'sports/about.html')
+
+def login(request):
+    return render(request,'sports/login.html')
+
+def forgetpass(request):
+    return render(request,'sports/forgotpass.html')
+
+
 
 
