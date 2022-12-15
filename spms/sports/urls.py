4209d7d435d14694040d0d6fc9232e0a2efc6b9e
asifalmahmud18898 <59229462+asifmahmud18898@users.noreply.github.com>
2022-12-16 00:37:29 +0600

    Changed

--- spms/sports/urls.py
+++ spms/sports/urls.py
@@ -4,7 +4,7 @@ from .import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('about/'views.about,name="about"),
+    path('about/',views.about,name="about"),
     
     path("AllCards", views.AllCards, name='AllCards'),
     path("admindashboard", views.admindashboard, name='admindashboard'),
