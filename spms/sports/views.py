3b4d36d9eb10429bd250e43fcae22ffbdb7f1a30
Hasnain <mrajhasnain101@gmail.com>
2022-12-18 02:15:18 +0600

    last commit before sleeping

--- spms/sports/views.py
+++ spms/sports/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 from .models import *
 from .forms import CreateUserForm
@@ -24,6 +25,8 @@ def registerAccount(request):
         form=CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request,'account was created for'+ user)
             return redirect('login')
     context={'form':form}
     return render(request,'sports/registerAccount.html',context)
