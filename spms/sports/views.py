1d128375f53748ee08bc11f24e7fb4a87ce9545b
Hasnain <mrajhasnain101@gmail.com>
2022-12-26 23:06:06 +0600

    views change

--- spms/sports/views.py
+++ spms/sports/views.py
@@ -28,8 +28,7 @@ def loginPage(request):
             return redirect('loginPage')
     return render(request,'sports/login.html')
 
-def forgotpass(request):
-    return render(request,'sports/forgotpass.html')
+
 
 def registerAccount(request):
     form=CreateUserForm(request.POST)
